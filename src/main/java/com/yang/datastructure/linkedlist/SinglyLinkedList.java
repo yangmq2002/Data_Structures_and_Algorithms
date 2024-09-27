a246c012976dec8088d6cd4e82139b637a65fe29
yang <1958523752@qq.com>
2024-09-27 20:53:52 +0800

    尾插法、根据索引查值

--- src/main/java/com/yang/datastructure/linkedlist/SinglyLinkedList.java
+++ src/main/java/com/yang/datastructure/linkedlist/SinglyLinkedList.java
@@ -14,26 +14,32 @@ public class SinglyLinkedList implements Iterable<Integer> {  // 整体
 
     @Override
     public Iterator<Integer> iterator() {
-        return new Iterator<Integer>() {
-            Node p = head;
-            @Override
-            public boolean hasNext() {
-                return p != null;
-            }
+        // 匿名内部类 -> 带名字的内部类
+        return new NodeIterator();
+    }
 
-            @Override
-            public Integer next() {  // 返回当前值，并指向下一个元素
-                int v = p.value;
-                p = p.next;
-                return v;
-            }
-        };
+    private class NodeIterator implements Iterator<Integer> {
+        Node p = head;  // 用到了外部类的成员变量，不能加static
+
+        @Override
+        public boolean hasNext() {
+            return p != null;
+        }
+
+        @Override
+        public Integer next() {  // 返回当前值，并指向下一个元素
+            int v = p.value;
+            p = p.next;
+            return v;
+        }
     }
 
     /**
      * 节点类
      */
     private static class Node {
+        // 内部的变量和外部没有关系，可以加 static
+        // 尽量加上
         int value; // 值
         Node next; // 下一个节点指针
 
@@ -45,6 +51,7 @@ public class SinglyLinkedList implements Iterable<Integer> {  // 整体
 
     /**
      * 向链表头部添加值
+     *
      * @param value: 待添加值
      */
     public void addFirst(int value) {
@@ -55,24 +62,82 @@ public class SinglyLinkedList implements Iterable<Integer> {  // 整体
     }
 
     /**
-     * 遍历列表
+     * 遍历列表1
      */
     public void loop1(Consumer<Integer> consumer) {
         Node p = head;
-        while(p != null) {
+        while (p != null) {
             consumer.accept(p.value);
             p = p.next;
         }
     }
 
     /**
-     * 遍历列表
-     * @param consumer
+     * 遍历列表2
+     *
+     * @param consumer: 要执行的操作
      */
     public void loop2(Consumer<Integer> consumer) {
-        for(Node p = head; p != null; p = p.next) {
+        for (Node p = head; p != null; p = p.next) {
             consumer.accept(p.value);
         }
+    }
+
+    /**
+     * 找到最后一个节点
+     *
+     * @return
+     */
+    private Node findLast() {
+
+        if(head == null) return null;  // 空链表
+
+        Node p = head;
+        for (p = head; p.next != null; p = p.next) {
+
+        }
+        return p;
+    }
+
+    /**
+     * 尾插法
+     * @param value
+     */
+    public void addLast(int value) {
+        Node last = findLast();
+
+        if (last == null) {
+            addFirst(value);
+            return;
+        }
+
+        last.next = new Node(value, null);
+    }
+
+    /**
+     * 根据所以索引查找
+     * @param index: 索引
+     * @return: 找到，返回该索引位置节点的值
+     * Throws: 找不到则抛异常
+     */
+    private Node findNode(int index) {
+        int i = 0;
+        for (Node p = head; p != null; p = p.next, i++) {
+            if (i == index) {
+                return p;
+            }
+        }
+        return null;  // 没找到
+    }
+
+    public int get(int index) {
+        Node p = findNode(index);
+
+        if (p == null) {
+            throw new IllegalArgumentException(String.format("Index %d is out of bounds", index));
+        }
+
+        return p.value;
     }
 
 }
--- src/test/java/com/yang/datastructer/linkedlist/TestSinglyLinkedList.java
+++ src/test/java/com/yang/datastructer/linkedlist/TestSinglyLinkedList.java
@@ -20,5 +20,32 @@ public class TestSinglyLinkedList {
 
     }
 
+    @Test
+    public void test2(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        list.addLast(5);
+//        list.loop2(System.out::println);
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+
+    }
+
+    @Test
+    public void test3(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        list.addLast(5);
+        System.out.println(list.get(2));
+        System.out.println(list.get(10));
+    }
+
 
 }
