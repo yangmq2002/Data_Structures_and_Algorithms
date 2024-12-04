46b5d017e835cb75aca40c19f310bd7241058aca
yang <1958523752@qq.com>
2024-12-04 20:57:49 +0800

    记忆法改进斐波那契数列

--- src/main/java/com/yang/algorithm/recursion_multi/E01Fibonacci.java
+++ src/main/java/com/yang/algorithm/recursion_multi/E01Fibonacci.java
@@ -2,25 +2,50 @@ package com.yang.algorithm.recursion_multi;
 
 import jdk.internal.org.jline.utils.OSUtils;
 
+import java.util.Arrays;
+
 /**
  * 递归求斐波那契数列第 n 项
  */
 public class E01Fibonacci {
 
-    public static int f(int n) {
-        if (n <=1 ) {
+    /**
+     * 使用 Memoization(记忆法，也称备忘录法) 改进
+     *
+     * @param n
+     * @return
+     */
+    public static int fibonacci(int n) {
+        int[] cache = new int[n + 1];
+        Arrays.fill(cache, -1);
+        cache[0] = 0;
+        cache[1] = 1;
+
+
+        return f(n, cache);
+    }
+
+
+    public static int f(int n, int[] cache) {
+        /*if (n <= 1) {
             return n;
-        }
-        return f(n-1) + f(n - 2);
+        }*/
+
+        if (cache[n] != -1)
+            return cache[n];
+
+        int x = f(n - 1, cache);
+        int y = f(n - 2, cache);
+        cache[n] = x+y;
+        return cache[n];
     }
 
     public static void main(String[] args) {
-        System.out.println(f(1));
-        System.out.println(f(0));
-        System.out.println(f(2));
-        System.out.println(f(3));
-        System.out.println(f(8));
+        System.out.println(fibonacci(1));
+        System.out.println(fibonacci(2));
+        System.out.println(fibonacci(8));
+        System.out.println(fibonacci(13));
 
-        }
+    }
 
 }
