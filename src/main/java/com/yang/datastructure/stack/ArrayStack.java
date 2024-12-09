a36f9d6dfb8da104e244bd8d09e18e0b59b84c68
yang <1958523752@qq.com>
2024-12-09 17:29:03 +0800

    栈-数组实现

---
+++ src/main/java/com/yang/datastructure/stack/ArrayStack.java
@@ -0,0 +1,63 @@
+package com.yang.datastructure.stack;
+
+import java.util.Iterator;
+
+public class ArrayStack<E> implements Stack<E>, Iterable<E> {
+
+    private E[] array;
+    private int top; // 栈顶指针
+
+    @SuppressWarnings("all")
+    public ArrayStack(int capacity) {
+        this.array = (E[]) new Object[capacity];
+    }
+
+    @Override
+    public boolean push(E value) {
+        if (isFull()) return false;
+        array[top++] = value;
+        return true;
+    }
+
+    @Override
+    public E pop() {
+        if (isEmpty()) return null;
+        E value = array[--top];
+
+        return value;
+    }
+
+    @Override
+    public E peek() {
+        if (isEmpty()) return null;
+
+        return array[top - 1];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return top == 0;
+    }
+
+    @Override
+    public boolean isFull() {
+        return top == array.length;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+
+            int p = top;
+            @Override
+            public boolean hasNext() {
+                return p > 0 ;
+            }
+
+            @Override
+            public E next() {
+                return array[--p];
+            }
+        };
+    }
+}
